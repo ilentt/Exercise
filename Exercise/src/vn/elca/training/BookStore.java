e53e3d7c11224142e09e8b6593500c01255e6c9f
ilt <ilentt@ilenlab.com>
2015-10-17 18:54:01 +0700

    update project

---
+++ Exercise/src/vn/elca/training/BookStore.java
@@ -0,0 +1,136 @@
+package vn.elca.training;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BookStore {
+
+	private Map<String, Set<Book>> m_bookStore = new HashMap<String, Set<Book>>();
+
+	public void addBook(Book book) {
+		if (!m_bookStore.containsKey(book.getM_author())) {
+			m_bookStore.put(book.getM_author(), new HashSet<Book>());
+		}
+
+		m_bookStore.get(book.getM_author()).add(book);
+	}
+
+	public Set<Book> getAllBooksOfAuthor(String author) {
+		return m_bookStore.get(author);
+	}
+
+	public Book getTheMostExpensiveBookOf(String author) {
+
+		List<Book> list = new ArrayList<Book>(m_bookStore.get(author));
+		Collections.sort(list, new Comparator<Book>() {
+
+			@Override
+			public int compare(Book o1, Book o2) {
+
+				return o2.getM_price() - o1.getM_price();
+			}
+
+		});
+
+		return list.get(0);
+
+	}
+	
+	public void runApp() {
+		BookStore b = new BookStore();
+		Book b1 = new Book("Henrry Poter", 551, "Harry", 5);
+		Book b2 = new Book("War and Peace", 416, "Tonstoive", 7);
+		Book b3 = new Book("Oliver Twist", 979, "Harry", 3);
+		Book b4 = new Book("The last leaf", 421, "Harry", 8);
+		Book b5 = new Book("Missisipi", 1254, "Harry", 2);
+		Book b6 = new Book("Gone a Wind", 685, "Tonstoive", 7);
+		Book b7 = new Book("Gone a Wind", 685, "Tonstoive", 7);
+
+		b.addBook(b1);
+		b.addBook(b2);
+		b.addBook(b3);
+		b.addBook(b4);
+		b.addBook(b5);
+		b.addBook(b6);
+		b.addBook(b7);
+		
+		for (Map.Entry<String, Set<Book>> book : b.m_bookStore.entrySet()) {
+			System.out.println(book.getValue().toString());
+		}
+		
+		System.out.println(b.getTheMostExpensiveBookOf("Harry"));
+	}
+
+	public static void main(String[] args) {
+
+		BookStore b = new BookStore();
+		b.runApp();
+	}
+}
+
+class Book {
+	private String m_name;
+	private int m_price;
+	private String m_author;
+	private int m_quantity;
+
+	public Book() {
+		// constructor
+	}
+
+	/**
+	 * @param m_name
+	 * @param m_price
+	 * @param m_author
+	 * @param m_quantity
+	 */
+	public Book(String m_name, int m_price, String m_author, int m_quantity) {
+		this.m_name = m_name;
+		this.m_price = m_price;
+		this.m_author = m_author;
+		this.m_quantity = m_quantity;
+	}
+
+	public String getM_name() {
+		return m_name;
+	}
+
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+
+	public int getM_price() {
+		return m_price;
+	}
+
+	public void setM_price(int m_price) {
+		this.m_price = m_price;
+	}
+
+	public String getM_author() {
+		return m_author;
+	}
+
+	public void setM_author(String m_author) {
+		this.m_author = m_author;
+	}
+
+	public int getM_quantity() {
+		return m_quantity;
+	}
+
+	public void setM_quantity(int m_quantity) {
+		this.m_quantity = m_quantity;
+	}
+
+	public String toString() {
+		return "[Author: " + this.m_author + ", Book: " + this.m_name + ", Price: " + this.m_price + ", Qut: "
+				+ this.m_quantity + "]\n";
+	}
+}
--- Exercise/src/vn/elca/training/Question10.java
+++ Exercise/src/vn/elca/training/Question10.java
@@ -1,6 +1,9 @@
 package vn.elca.training;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,16 +15,25 @@ public class Question10 {
 
 	public static void sortDates(List<DAY> dayList) {
 		Set<DAY> set = new TreeSet<DAY>(dayList);
-		
+
 		/*
+		 * List<DAY> list = new ArrayList<DAY>(set); Collections.sort(list,
+		 * Collections.reverseOrder()); set = new LinkedHashSet<DAY>(list);
+		 */
 		List<DAY> list = new ArrayList<DAY>(set);
-		Collections.sort(list, Collections.reverseOrder());
-		set = new LinkedHashSet<DAY>(list);
-		*/
-		for (DAY day : set) {
-			if(day.ordinal() == 6) {
-				System.out.println(day.ordinal());
+		Collections.sort(list, new Comparator<DAY>() {
+
+			@Override
+			public int compare(DAY d1, DAY d2) {
+				// TODO Auto-generated method stub
+				if (d1.equals(DAY.SUN)) {
+					return -1;
+				}
+				return d1.ordinal() - d2.ordinal();
 			}
+		});
+		for (DAY day : list) {
+			System.out.println(day);
 		}
 
 	}
@@ -31,7 +43,7 @@ enum DAY {
 	MON("Monday"), TUE("Tuesday"), WED("Wednesday"), THU("Thurday"), FRI("Friday"), SAT("Saturday"), SUN("Sunday");
 
 	String m_name;
-	
+
 	DAY(String name) {
 		m_name = name;
 	}
